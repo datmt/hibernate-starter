8059ccaf0d7f4f45359ffaecc8cd57e59b414c51
dtt <t2dx.inc@gmail.com>
2022-06-24 10:52:39 +0700

    first commit

---
+++ src/main/java/com/datmt/hibernate/Main.java
@@ -0,0 +1,27 @@
+package com.datmt.hibernate;
+
+import com.datmt.hibernate.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    private static final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private static final SessionFactory factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    public static void main(String[] args) {
+        try (Session session = factory.openSession())         {
+            Transaction tx = session.beginTransaction();
+
+            User user = new User();
+            user.setName("Jane");
+            user.setAge(10000);
+
+            session.persist(user);
+            tx.commit();
+        }
+    }
+}
---
+++ src/main/java/com/datmt/hibernate/model/User.java
@@ -0,0 +1,40 @@
+package com.datmt.hibernate.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
